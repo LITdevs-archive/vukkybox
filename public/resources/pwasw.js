e85f4e6e05adc70d99375ee6c6aa471839c0b054
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-12-06 09:24:44 +0100

    add music for a full gallery

--- public/resources/pwasw.js
+++ public/resources/pwasw.js
@@ -1,15 +1,17 @@
 caches.delete('v1');
 caches.delete('v2');
 caches.delete('v3');
+caches.delete('v4');
 
 //this function triggers when the user clicks the Install app button.
 self.addEventListener('install', (event)=>{
   event.waitUntil(
-    caches.open('v4').then((cache)=>{
+    caches.open('v5').then((cache)=>{
       return cache.addAll([
         "/resources/offline.html",
         "/resources/loading.ogg",
         "/resources/gallery.ogg",
+        "/resources/gallerydone.ogg",
         "/resources/shop.ogg"
       ]);
     })
