0a885eb7ad13afe1cc93fb22d59b9d6fb59fba18
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-12-07 13:16:01 +0100

    prefers reduced motion

--- public/resources/spin.js
+++ public/resources/spin.js
@@ -1,12 +1,15 @@
 window.addEventListener('load', (event) => {
-    icon.classList.add("duration-150");
-    icon.onclick = function(){
-        icon.classList.add("animate-ping");
-        setTimeout(() => {
-            icon.classList.remove("animate-ping");
-            icon.style.display = "none";
-        }, 200);
-    };
+    const noAnim = !window.matchMedia("(prefers-reduced-motion: reduce)") || window.matchMedia("(prefers-reduced-motion: reduce)").matches
+    if(!noAnim) {
+        icon.classList.add("duration-150");
+        icon.onclick = function(){
+            icon.classList.add("animate-ping");
+            setTimeout(() => {
+                icon.classList.remove("animate-ping");
+                icon.style.display = "none";
+            }, 200);
+        };
+    }
     document.querySelector("a[href='/']").parentNode.querySelector("p").id = "balance";
     document.querySelector("#balance").onclick = function() {
         document.location.pathname = "balance";
