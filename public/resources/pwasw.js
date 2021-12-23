73595fbb3a002c3bcc289d618420d9c11f3a13fb
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-12-23 15:55:02 +0100

    i'm not sure if i did this but i want to be sure anyways

--- public/resources/pwasw.js
+++ public/resources/pwasw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', (event)=>{
       for (let name of names)
           caches.delete(name);
     }),
-    caches.open('v18').then((cache)=>{
+    caches.open('v19').then((cache)=>{
       return cache.addAll([ // the secrets list :(
         "/resources/spin.js",
         "/resources/offline.html",
