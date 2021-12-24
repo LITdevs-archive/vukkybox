bc90097b38a388a323541ce4bcb26aa0cac1d227
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-12-24 14:19:48 +0100

    well ok then

--- public/resources/reskin/corrupted.js
+++ public/resources/reskin/corrupted.js
@@ -1,7 +1,2 @@
 document.querySelector("nav").remove();
 document.body.classList = [];
-window.addEventListener('load', (event) => {
-    document.querySelectorAll("button").forEach(function(node) {
-        node.remove();
-    })
-});
