1388fd742d4e54361aac4b8632730a1a5fa9a03d
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-12-28 21:32:54 +0100

    credits music.mp3

--- public/resources/pwasw.js
+++ public/resources/pwasw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', (event)=>{
       for (let name of names)
           caches.delete(name);
     }),
-    caches.open('v22').then((cache)=>{
+    caches.open('v23').then((cache)=>{
       return cache.addAll([ // the secrets list :(
         "/resources/spin.js",
         "/resources/offline.html",
@@ -37,7 +37,8 @@ self.addEventListener('install', (event)=>{
         "/resources/imagemap.js",
         "/resources/unboxcrusty.ogg",
         "/resources/icons/santa.png",
-        "/resources/beggarsvukkybox.webp"
+        "/resources/beggarsvukkybox.webp",
+        "/resources/credits.ogg"
       ]);
     })
   );
