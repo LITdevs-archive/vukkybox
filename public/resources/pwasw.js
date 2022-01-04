6abb8edda77cb3bfbbea4065004381fb6fc46b4c
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-01-04 09:40:11 +0200

    a

--- public/resources/pwasw.js
+++ public/resources/pwasw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', (event)=>{
       for (let name of names)
           caches.delete(name);
     }),
-    caches.open('v31').then((cache)=>{
+    caches.open('v32').then((cache)=>{
       return cache.addAll([ // the secrets list :(
         "/resources/spin.js",
         "/resources/offline.html",
