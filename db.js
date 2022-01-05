51f66fcbebf21c4789294a0d5e3b3f34d8de3794
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-01-05 14:19:30 +0200

    okay that broke everything so lets do it like this instread

--- db.js
+++ db.js
@@ -491,8 +491,6 @@ function vukkyTierCount(vukkies) {
 				} else {
 					theOutput[rarity[0]] = 1;
 				}
-			} else {
-				theOutput[rarity[0]] = 0;
 			}
 		})
 	})
@@ -620,8 +618,8 @@ function leaderboard(req, user, callback) { // req: {board: board, limit: 10/50/
 	} else {
 		User.find({}, function(err, allUsers){
 			let allUsersVukkiesInTier = []
-			console.log(vukkyTierCount(user._id ? user.gallery : user[0].gallery)[req.rarity])
-			if(vukkyTierCount(user._id ? user.gallery : user[0].gallery)[req.rarity] < 1) getUserRank = false
+			let actualUserRarity = vukkyTierCount(user._id ? user.gallery : user[0].gallery)[req.rarity] < 1
+			if(!actualUserRarity || isNaN(actualUserRarity) || actualUserRarity < 1) getUserRank = false
 			for (let i = 0; i < allUsers.length; i++) {
 				let userRarity = vukkyTierCount(allUsers[i].gallery)[req.rarity];
 				if (userRarity >= 1) allUsersVukkiesInTier.push({username: allUsers[i].username.includes("@") ? "Username Hidden for Privacy" : allUsers[i].username, userId: allUsers[i]._id, data: userRarity})
