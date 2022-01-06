13efb5415bc6bc1a07d9b7270ad471bd857a39bc
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-01-06 14:14:23 +0200

    a

--- app.js
+++ app.js
@@ -460,6 +460,7 @@ app.get('/stats', grl, checkAuth, popupMid, function(req, res) {
 
 app.get('/beta', grl, checkAuth, popupMid, function(req, res) {
 	if(req.headers.referer == "https://vukkybox.com/credits") return res.render(__dirname + '/public/beta.ejs', {csrfToken: req.csrfToken()})
+	if(req.headers.referer == "https://vukkybox.com/admin") return res.render(__dirname + '/public/beta.ejs', {csrfToken: req.csrfToken()})
 	res.status(404).render(`${__dirname}/public/404.ejs`);
 })
 
