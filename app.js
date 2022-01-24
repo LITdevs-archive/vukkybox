edfaf8a54c64a88f708a5b9b8d1b19f59b298723
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-01-24 16:28:10 +0200

    add db getuser call to make sure the information is up to date

--- app.js
+++ app.js
@@ -681,13 +681,15 @@ app.get('/translog', grl, checkAuth, function(req, res) {
 
 app.get('/2fa', grl, checkAuth, function(req, res) {
 	let user = req.user._id ? req.user : req.user[0];
-	if(user.twoFactor) return res.render(`${__dirname}/public/2fareset.ejs`, {user: user, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});
-	let secret = speakeasy.generateSecret({name: "Vukkybox 2FA"});
-	req.session.two_factor_temp_secret = secret.base32;
-	qrcode.toDataURL(secret.otpauth_url, function(err, dataUrl) {
-		if (err) return res.render(__dirname + '/public/error.ejs', {stacktrace: null, friendlyError: "Something went wrong while starting the 2FA flow. <br>For your privacy the stacktrace is hidden, if this happens again please contact us."});
-		res.render(`${__dirname}/public/2fa.ejs`, {csrfToken: req.csrfToken(), user: user, qrDataUrl: dataUrl, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});
-	});
+	db.getUser(user._id, user => {
+		if(user.twoFactor) return res.render(`${__dirname}/public/2fareset.ejs`, {user: user, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});
+		let secret = speakeasy.generateSecret({name: "Vukkybox 2FA"});
+		req.session.two_factor_temp_secret = secret.base32;
+		qrcode.toDataURL(secret.otpauth_url, function(err, dataUrl) {
+			if (err) return res.render(__dirname + '/public/error.ejs', {stacktrace: null, friendlyError: "Something went wrong while starting the 2FA flow. <br>For your privacy the stacktrace is hidden, if this happens again please contact us."});
+			res.render(`${__dirname}/public/2fa.ejs`, {csrfToken: req.csrfToken(), user: user, qrDataUrl: dataUrl, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});
+		});
+	})
 });
 
 app.post('/fotp', checkAuth, function(req, res) {
