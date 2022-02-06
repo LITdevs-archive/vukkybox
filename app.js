427c526f1afcb0f4260ca2bac8d45881003a5717
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-02-06 17:31:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -763,7 +763,8 @@ app.get('/2fa', grl, checkAuth, function(req, res) {
 	})
 });
 
-app.get('/validate2fa', grl, checkAuth, function(req, res) {
+app.get('/validate2fa', grl, function(req, res) {
+	if (!req.isAuthenticated) return res.redirect("/login");
 	let user = req.user._id ? req.user : req.user[0];
 	db.getUser(user._id, user => {
 		if(!user.twoFactor) return res.send("you dont even have 2FA enabled lol");
