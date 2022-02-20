d36a76de5a03457d78bd309e8309cdbf7e8b5b63
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-02-20 13:31:59 +0100

    disallow guestgallery

--- app.js
+++ app.js
@@ -962,7 +962,7 @@ app.get('/humans.txt', function(req, res){
 
 app.get('/robots.txt', function (req, res) {
     res.type('text/plain');
-    res.send("# All robots allowed\n# Unless you're a rude one\n\nUser-agent: *\nDisallow:");
+    res.send("# All robots allowed\n# Unless you're a rude one\n\nUser-agent: *\nDisallow: /guestgallery/*");
 });
 
 app.get('/.well-known/keybase.txt', function (req, res) {
