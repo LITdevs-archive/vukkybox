3db41ea0d8d9dd433f1cb68371a7bd7071b26b2f
Vukky <vul.andreas@gmail.com>
2022-06-08 13:14:26 +0200

    grr

--- public/resources/pwasw.js
+++ public/resources/pwasw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', (event)=>{
       for (let name of names)
           caches.delete(name);
     }),
-    caches.open('v51').then((cache)=>{
+    caches.open('v52').then((cache)=>{
       return cache.addAll([ // the secrets list :(
         "/resources/spin.js",
         "/resources/offline.html",
